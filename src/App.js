d285e70e2e2557b84604652401275e0e34f9b56c
Renee Mumbi <1723renee@gmail.com>
2023-03-01 16:25:47 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Goodluck on your project
         </a>
       </header>
     </div>
